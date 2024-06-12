32bbdf7d29ec507cc90d89bde364295d58138ad1
EMPEROR <sahilsorte8457@gmail.com>
2024-06-13 01:03:57 +0530

    Show Interview Question

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -6,23 +6,21 @@ import { eq } from 'drizzle-orm';
 import Webcam from 'react-webcam';
 import { Lightbulb, WebcamIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 function Interview({ params }) {
     const [interviewData, setInterviewData] = useState();
     const [webCamEnabled, setWebCamEnabled] = useState(false);
 
     useEffect(() => {
-        console.log(params.interviewId)
         GetInterviewDetails();
     }, [])
 
     //Used to get interview details by mockId/interviewId
     const GetInterviewDetails = async () => {
         const result = await db.select().from(MockInterview).where(eq(MockInterview.mockId, params.interviewId));
-
-        console.log(result[0]);
-
         setInterviewData(result[0]);
+        // console.log(result[0]);
     }
     return (
         <div className='my-10'>
@@ -30,9 +28,9 @@ function Interview({ params }) {
             <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
                 <div className='flex flex-col my-5 gap-5'>
                     <div className='flex flex-col p-5 rounded-lg border gap-5'>
-                        <h2 className='text-lg'><strong>Job Role/Job Position: </strong>{interviewData.jobPosition}</h2>
-                        <h2 className='text-lg'><strong>Job Description/Tech Stack: </strong>{interviewData.jobDesc}</h2>
-                        <h2 className='text-lg'><strong>Years of Experience: </strong>{interviewData.jobExperience}</h2>
+                        {interviewData && <h2 className='text-lg'><strong>Job Role/Job Position: </strong>{interviewData.jobPosition}</h2>}
+                        {interviewData && <h2 className='text-lg'><strong>Job Description/Tech Stack: </strong>{interviewData.jobDesc}</h2>}
+                        {interviewData && <h2 className='text-lg'><strong>Years of Experience: </strong>{interviewData.jobExperience}</h2>}
                     </div>
                     <div className='p-5 border rounded-lg border-yellow-300 bg-yellow-100'>
                         <h2 className='flex gap-2 items-center text-yellow-600'><Lightbulb /><strong>Information</strong></h2>
@@ -52,7 +50,7 @@ function Interview({ params }) {
                 </div>
             </div>
             <div className='flex justify-end items-end'>
-                <Button>Start Interview</Button>
+                <Link href={'/dashboard/interview/' + params.interviewId + '/start'}><Button>Start Interview</Button></Link>
             </div>
         </div>
     )
---
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -0,0 +1,45 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import { db } from '@/utils/db';
+import { MockInterview } from '@/utils/schema';
+import { eq } from 'drizzle-orm';
+import QuestionsSection from './_components/QuestionsSection';
+import RecordAnswerSection from './_components/RecordAnswerSection';
+function StartInterview({ params }) {
+
+  const [interviewData, setInterviewData] = useState();
+  const [MockInterviewQuestion, setMockInterviewQuestion] = useState();
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+
+  useEffect(() => {
+    GetInterviewDetails();
+  }, []);
+
+  //Used to get interview details by mockId/interviewId
+  const GetInterviewDetails = async () => {
+    const result = await db.select().from(MockInterview).where(eq(MockInterview.mockId, params.interviewId));
+
+    const jsonMockResp = JSON.parse(result[0].jsonMockResp);
+
+    console.log(jsonMockResp);
+
+    setMockInterviewQuestion(jsonMockResp);
+
+    setInterviewData(result[0]);
+    // console.log(result[0]);
+  }
+
+  return (
+    <div>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+        <QuestionsSection
+          mockInterviewQuestion={MockInterviewQuestion}
+          activeQuestionIndex={activeQuestionIndex}
+        />
+        <RecordAnswerSection />
+      </div>
+    </div>
+  )
+}
+
+export default StartInterview
