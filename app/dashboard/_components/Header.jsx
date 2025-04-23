e0cf2cf704213fe4206f1e4e23f41f154d8fbbba
Sahil Sorte <129893605+Sahil-pvt@users.noreply.github.com>
2025-04-23 19:43:56 +0530

    Minor update

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -15,14 +15,14 @@ function Header() {
         <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
             <Image src={'/logo.svg'} alt='prep-n-hire logo' width={160} height={100} />
             <ul className='hidden md:flex gap-6'>
-                <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+                {/* <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
                 ${path == '/dashboard' && 'text-primary font-bold'}`}>Dashboard</li>
                 <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
                 ${path == '/dashboard/questions' && 'text-primary font-bold'}`}>Questions</li>
                 <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
                 ${path == '/dashboard/upgrade' && 'text-primary font-bold'}`}>Upgrade</li>
                 <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
-                ${path == '/dashboard/how' && 'text-primary font-bold'}`}>How it works?</li>
+                ${path == '/dashboard/how' && 'text-primary font-bold'}`}>How it works?</li> */}
             </ul>
             <UserButton />
         </div>
